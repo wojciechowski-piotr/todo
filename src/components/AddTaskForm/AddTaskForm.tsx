3527b8d331575cac6be172e9cf7414febe8ad6b1
wojciechowski-piotr <pwojciechowski2014@gmail.com>
2021-01-05 11:31:01 +0100

    validate input added

--- src/components/AddTaskForm/AddTaskForm.tsx
+++ src/components/AddTaskForm/AddTaskForm.tsx
@@ -1,36 +1,57 @@
-import React, { FormEvent, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import { FormEvent, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { add } from '../../actions/tasks';
 
-import styles from './AddTaskForm.module.scss'
+import styles from './AddTaskForm.module.scss';
 
-interface Props {}
+const AddTaskForm = () => {
+    const tasks = useSelector((state: any) => state);
 
-const AddTaskForm = (props: Props) => {
-    const inputRef = useRef<HTMLInputElement | any>();
+    const buttonRef = useRef<HTMLButtonElement | any>();
+
+    const [taskInput, setTaskInput] = useState('');
+    const [isValid, setIsValid] = useState(false);
 
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        const validate = () => {
+            if (taskInput.trim() !== '') {
+                setIsValid(true);
+                buttonRef.current.disabled = false;
+
+                return;
+            }
+
+            setIsValid(false);
+            buttonRef.current.disabled = true;
+        };
+        validate();
+    }, [taskInput]);
+
+    const handleChange = (event: any) => {
+        setTaskInput(event.target.value);
+    };
+
     const handleSubmit = (event: FormEvent) => {
-        const inputString = inputRef.current.value;
-
-        if (inputString.trim() === '') {
-            alert('Type something');
-        } else {
-            dispatch(add(inputRef.current.value));
-            inputRef.current.value = '';
-        }
-
         event.preventDefault();
+
+        if (isValid === true) {
+            dispatch(add(taskInput));
+            setTaskInput('');
+        }
     };
 
     return (
         <>
-            <form onSubmit={handleSubmit} className={styles.root}>
-                <input ref={inputRef} type="text" placeholder='Enter a task...' />
-                <button type="submit">Add</button>
+            <form onSubmit={handleSubmit} className={styles.root} style={isValid ? { marginBottom: '2rem' } : { marginBottom: '0.5rem' }}>
+                <input value={taskInput} onChange={handleChange} type="text" placeholder="Enter the task name..." />
+                <button ref={buttonRef} type="submit">
+                    Add
+                </button>
             </form>
+            {isValid === false && tasks.length !== 0 && <div className={styles.warn}>You have to enter something to add a task</div>}
         </>
     );
 };
